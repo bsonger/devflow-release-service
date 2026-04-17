542e246bbf523337d046e317fc87ae0781233327
bei song <bsonger@163.com>
2026-04-18 04:37:44 +0800

    fix: make image rename migration idempotent

--- migrations/0002_rename_manifests_to_images.sql
+++ migrations/0002_rename_manifests_to_images.sql
@@ -1,12 +1,59 @@
 BEGIN;
 
-ALTER TABLE IF EXISTS manifests RENAME TO images;
+DO $$
+BEGIN
+  IF EXISTS (
+    SELECT 1
+    FROM information_schema.tables
+    WHERE table_schema = 'public' AND table_name = 'manifests'
+  ) AND NOT EXISTS (
+    SELECT 1
+    FROM information_schema.tables
+    WHERE table_schema = 'public' AND table_name = 'images'
+  ) THEN
+    ALTER TABLE manifests RENAME TO images;
+  END IF;
+END $$;
 
-ALTER INDEX IF EXISTS manifests_pkey RENAME TO images_pkey;
-ALTER INDEX IF EXISTS uq_manifests_application_name_active RENAME TO uq_images_application_name_active;
-ALTER INDEX IF EXISTS uq_manifests_pipeline_id_active RENAME TO uq_images_pipeline_id_active;
-ALTER INDEX IF EXISTS idx_manifests_application_id RENAME TO idx_images_application_id;
-ALTER INDEX IF EXISTS idx_manifests_execution_intent_id RENAME TO idx_images_execution_intent_id;
+DO $$
+BEGIN
+  IF EXISTS (SELECT 1 FROM pg_class WHERE relkind = 'i' AND relname = 'manifests_pkey')
+     AND NOT EXISTS (SELECT 1 FROM pg_class WHERE relkind = 'i' AND relname = 'images_pkey') THEN
+    ALTER INDEX manifests_pkey RENAME TO images_pkey;
+  END IF;
+END $$;
+
+DO $$
+BEGIN
+  IF EXISTS (SELECT 1 FROM pg_class WHERE relkind = 'i' AND relname = 'uq_manifests_application_name_active')
+     AND NOT EXISTS (SELECT 1 FROM pg_class WHERE relkind = 'i' AND relname = 'uq_images_application_name_active') THEN
+    ALTER INDEX uq_manifests_application_name_active RENAME TO uq_images_application_name_active;
+  END IF;
+END $$;
+
+DO $$
+BEGIN
+  IF EXISTS (SELECT 1 FROM pg_class WHERE relkind = 'i' AND relname = 'uq_manifests_pipeline_id_active')
+     AND NOT EXISTS (SELECT 1 FROM pg_class WHERE relkind = 'i' AND relname = 'uq_images_pipeline_id_active') THEN
+    ALTER INDEX uq_manifests_pipeline_id_active RENAME TO uq_images_pipeline_id_active;
+  END IF;
+END $$;
+
+DO $$
+BEGIN
+  IF EXISTS (SELECT 1 FROM pg_class WHERE relkind = 'i' AND relname = 'idx_manifests_application_id')
+     AND NOT EXISTS (SELECT 1 FROM pg_class WHERE relkind = 'i' AND relname = 'idx_images_application_id') THEN
+    ALTER INDEX idx_manifests_application_id RENAME TO idx_images_application_id;
+  END IF;
+END $$;
+
+DO $$
+BEGIN
+  IF EXISTS (SELECT 1 FROM pg_class WHERE relkind = 'i' AND relname = 'idx_manifests_execution_intent_id')
+     AND NOT EXISTS (SELECT 1 FROM pg_class WHERE relkind = 'i' AND relname = 'idx_images_execution_intent_id') THEN
+    ALTER INDEX idx_manifests_execution_intent_id RENAME TO idx_images_execution_intent_id;
+  END IF;
+END $$;
 
 DO $$
 BEGIN
@@ -23,6 +70,12 @@ BEGIN
   END IF;
 END $$;
 
-ALTER INDEX IF EXISTS idx_releases_manifest_id RENAME TO idx_releases_image_id;
+DO $$
+BEGIN
+  IF EXISTS (SELECT 1 FROM pg_class WHERE relkind = 'i' AND relname = 'idx_releases_manifest_id')
+     AND NOT EXISTS (SELECT 1 FROM pg_class WHERE relkind = 'i' AND relname = 'idx_releases_image_id') THEN
+    ALTER INDEX idx_releases_manifest_id RENAME TO idx_releases_image_id;
+  END IF;
+END $$;
 
 COMMIT;
